8ca354af486a1e7d000d5347fe6ee3aa54fd7b42
toono <52028799+toono-f@users.noreply.github.com>
2024-07-03 14:58:29 +0900

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+import { getData } from "@/actions/todoActions";
+import Todos from "@/components/Todos";
+
+export default async function Home() {
+  const data = await getData();
+  return <Todos todos={data} />;
+}
