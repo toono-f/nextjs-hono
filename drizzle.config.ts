7e4f050df77289a54c9da6ff1124165958072346
toono-f <52028799+toono-f@users.noreply.github.com>
2024-07-03 16:27:03 +0900

    srcディレクトリを解除

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,8 +2,8 @@ import "dotenv/config";
 import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-  schema: "./src/db/schema.ts",
-  out: "./src/drizzle",
+  schema: "./db/schema.ts",
+  out: "./drizzle",
   dialect: "postgresql",
   dbCredentials: {
     url: process.env.NEON_DATABASE_URL!,
